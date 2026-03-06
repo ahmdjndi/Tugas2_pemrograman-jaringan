f898c51c6d665e34ef99302e1a59442e2f8a5a10
ahmdjndi <ajunaidisatu@gmail.com>
2026-03-06 13:04:22 +0700

    Add files via upload

---
+++ JavaInputStream.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import javax.swing.*;
+
+public class JavaInputStream {
+    public static void main(String args[]) {
+
+        try {
+            JFileChooser chooser = new JFileChooser();
+            chooser.showOpenDialog(null);
+            File file = chooser.getSelectedFile();
+
+            InputStream fileInput = new FileInputStream(file);
+
+            int data;
+            while ((data = fileInput.read()) != -1) {
+                System.out.write(data);
+            }
+
+            fileInput.close();
+
+        } catch (IOException e) {
+            System.out.println("Terjadi kesalahan Pada Sistem!");
+        }
+    }
+}
