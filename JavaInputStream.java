911b4fab574f59415b306104145255bdb36b6ce9
ahmdjndi <ajunaidisatu@gmail.com>
2026-03-06 12:40:38 +0700

    Update JavaInputStream.java

--- JavaInputStream.java
+++ JavaInputStream.java
@@ -3,7 +3,7 @@ package com.pemrogramanJaringan.pemrogramanJaringan.Stream;
 import java.io.*;
 import javax.swing.*;
 
-public class CountHuruf {
+public class JavaInputStream {
     public static void main(String args[]) {
 
         try {
@@ -11,37 +11,14 @@ public class CountHuruf {
             chooser.showOpenDialog(null);
             File file = chooser.getSelectedFile();
 
-            FileInputStream openFile = new FileInputStream(file);
+            InputStream fileInput = new FileInputStream(file);
 
             int data;
-            int a = 0, i = 0, u = 0, e = 0, o = 0;
-
-            // hitung jumlah huruf a i u e o
-            while ((data = openFile.read()) != -1) {
-                char huruf = Character.toLowerCase((char) data);
-                if (huruf == 'a')
-                    a++;
-                else if (huruf == 'i')
-                    i++;
-                else if (huruf == 'u')
-                    u++;
-                else if (huruf == 'e')
-                    e++;
-                else if (huruf == 'o')
-                    o++;
+            while ((data = fileInput.read()) != -1) {
+                System.out.write(data);
             }
 
-            // tutup
-            openFile.close();
-
-            // tampilkan hasil huruf vokal yang sudah di hitung
-            JOptionPane.showMessageDialog(null,
-                    "Jumlah Huruf Vokal:\n" +
-                            "a = " + a + "\n" +
-                            "i = " + i + "\n" +
-                            "u = " + u + "\n" +
-                            "e = " + e + "\n" +
-                            "o = " + o);
+            fileInput.close();
 
         } catch (IOException e) {
             System.out.println("Terjadi kesalahan Pada Sistem!");
